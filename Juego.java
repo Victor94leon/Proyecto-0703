3e086d9152357a3e81eafc03514f4854da60411b
Víctor <victorlopez94leon@gmail.com>
2016-02-05 13:36:44 +0100

    Añadidos los métodos de la clase Juego

--- Juego.java
+++ Juego.java
@@ -1,33 +1,69 @@
-
-/**
- * Write a description of class Juego here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.ArrayList;
 public class Juego
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // ArrayList para guardar información sobre los jugadores que hay
+    private ArrayList<Jugador> jugadores;
+    // Objeto de tipo Mazo para guardar información sobre el mazo
+    private Mazo mazo;
 
     /**
-     * Constructor for objects of class Juego
+     * Constructor que crea un objeto Juego, el cual crea el numero de jugadores indicados y crea el mazo.
+     * Si el número de jugadores no es válido se crea un juego con 4 jugadores por defecto.
      */
-    public Juego()
+    public Juego(int numeroJugadores)
     {
-        // initialise instance variables
-        x = 0;
+        ArrayList<Jugador> jugadores = new ArrayList<Jugador>();    
+        mazo = new Mazo();
+        mazo.barajar();
+        int index = 1;  
+        if (numeroJugadores<0 || numeroJugadores>8) {
+            while (index<=4) {
+                Jugador nuevoJugador = new Jugador(index);
+                jugadores.add(nuevoJugador);
+                index++;
+            }
+        }
+        else {
+            while (index<=numeroJugadores) {
+                Jugador nuevoJugador = new Jugador(index);
+                jugadores.add(nuevoJugador);
+                index++;
+            }
+        }
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Método que reparte todas las cartas a los jugadores en la forma habitual en que se hace en un juego de cartas, 
+     * es decir, barajando el mazo y entregando alternativamente cartas a los jugadores tomando siempre la primera 
+     * carta del mazo. Al finalizar el reparto todos los jugadores deben tener el mismo número de cartas, por lo que 
+     * es posible, dependiendo del número de estos, que queden cartas en el mazo.
      */
-    public int sampleMethod(int y)
+    public void repartir()
     {
-        // put your code here
-        return x + y;
+        if (mazo.quedan()%jugadores.size() == 0) {
+            while (mazo.quedan()!=0) {
+                for (Jugador jugadorEnLista : jugadores) {
+                    jugadorEnLista.recibirCarta(mazo.tomarPrimera());
+                }
+            }
+        }
+        else {
+            while (mazo.quedan()!= mazo.quedan()%jugadores.size()) {
+                for (Jugador jugadorEnLista : jugadores) {
+                    jugadorEnLista.recibirCarta(mazo.tomarPrimera());
+                }
+            }
+        }
+    }
+
+    /**
+     * Método que muestra por pantalla el id de cada jugador junto a las cartas que tiene en la mano.
+     */
+    public void mostrarCartasJugadores()
+    {
+        for (Jugador jugadorEnLista : jugadores) {
+            System.out.println("Jugador: " + jugadorEnLista.getId());
+            System.out.println("Mano: " + jugadorEnLista.cartasQueTieneEnLaMano());
+        }
     }
 }
